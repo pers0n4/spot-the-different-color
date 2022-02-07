9a78c99bc758785db4ed71e614c0f4b643e6681f
Dong-Young Kim <31337.persona@gmail.com>
2022-02-08 01:50:28 +0900

    feat: define initial states

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function App() {
-  return <div></div>;
+  const [stage, setStage] = React.useState(1);
+  const [score, setScore] = React.useState(0);
+  const [timeLimit, setTimeLimit] = React.useState(15);
+
+  const tileCount = React.useMemo(
+    () => Math.pow(Math.round((stage + 0.5) / 2) + 1, 2),
+    [stage],
+  );
+
+  return (
+    <div>
+      스테이지: {stage}, 남은 시간: {timeLimit}, 점수: {score}
+      {Array.from({ length: tileCount }, (_, i) => (
+        <div key={i}>{i}</div>
+      ))}
+    </div>
+  );
 }
