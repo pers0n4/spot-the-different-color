ad9f3926e4bdcb3bfd636ddc13e7baabd6a3584f
Dong-Young Kim <31337.persona@gmail.com>
2022-02-10 02:23:21 +0900

    refactor: refactor useState to useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,66 @@ import Board from "./components/Board";
 import Header from "./components/Header";
 import "./App.css";
 
+type State = {
+  score: number;
+  stage: number;
+  timeLimit: number;
+};
+
+type Action =
+  | { type: "RESET" }
+  | { type: "NEXT_STAGE" }
+  | { type: "CHANGE_TIME"; payload: number };
+
+const initialState: State = {
+  score: 0,
+  stage: 1,
+  timeLimit: 15,
+};
+
+const reducer = (state: State, action: Action) => {
+  const { score, stage, timeLimit } = state;
+  switch (action.type) {
+    case "RESET":
+      return initialState;
+    case "NEXT_STAGE":
+      return {
+        score: score + Math.pow(stage, 3) * timeLimit,
+        stage: stage + 1,
+        timeLimit: 15,
+      };
+    case "CHANGE_TIME":
+      const time = timeLimit + action.payload;
+      return {
+        ...state,
+        timeLimit: time > 0 ? time : 0,
+      };
+  }
+};
+
 export default function App() {
-  const [stage, setStage] = React.useState(1);
-  const [score, setScore] = React.useState(0);
-  const [timeLimit, setTimeLimit] = React.useState(15);
+  const [{ score, stage, timeLimit }, dispatch] = React.useReducer(
+    reducer,
+    initialState,
+  );
 
   const handleCorrect = () => {
-    setScore((currentScore) => currentScore + Math.pow(stage, 3) * timeLimit);
-    setStage((currentState) => currentState + 1);
-    setTimeLimit(15);
+    dispatch({ type: "NEXT_STAGE" });
   };
 
   const handleIncorrect = () => {
-    setTimeLimit((currentTimeLimit) =>
-      currentTimeLimit - 3 > 0 ? currentTimeLimit - 3 : 0,
-    );
+    dispatch({ type: "CHANGE_TIME", payload: -3 });
   };
 
   React.useEffect(() => {
     if (timeLimit <= 0) {
       window.alert(`GAME OVER!\n스테이지: ${stage}, 점수: ${score}`);
-      setScore(0);
-      setStage(1);
-      setTimeLimit(15);
+      dispatch({ type: "RESET" });
     }
-    const timer = setTimeout(() => setTimeLimit((time) => time - 1), 1000);
+    const timer = setTimeout(
+      () => dispatch({ type: "CHANGE_TIME", payload: -1 }),
+      1000,
+    );
     return () => clearTimeout(timer);
   }, [timeLimit]);
 
