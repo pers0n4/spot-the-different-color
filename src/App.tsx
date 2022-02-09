97c4ff77ec8169d6b3bef3146751c9f5638f528f
Dong-Young Kim <31337.persona@gmail.com>
2022-02-09 19:03:46 +0900

    feat: add `Board` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import React from "react";
+import Board from "./components/Board";
 
 export default function App() {
   const [stage, setStage] = React.useState(1);
   const [score, setScore] = React.useState(0);
   const [timeLimit, setTimeLimit] = React.useState(15);
 
-  const tileCount = React.useMemo(
-    () => Math.pow(Math.round((stage + 0.5) / 2) + 1, 2),
-    [stage],
-  );
-
   return (
     <div>
       스테이지: {stage}, 남은 시간: {timeLimit}, 점수: {score}
-      {Array.from({ length: tileCount }, (_, i) => (
-        <div key={i}>{i}</div>
-      ))}
+      <Board stage={stage} />
     </div>
   );
 }
---
+++ src/components/Board.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Tile from "./Tile";
+
+interface Props {
+  stage: number;
+}
+
+export default function Board({ stage }: Props) {
+  const [tileCount, targetTileId] = React.useMemo(() => {
+    const count = Math.pow(Math.round((stage + 0.5) / 2) + 1, 2);
+    const target = Math.floor(Math.random() * count);
+    return [count, target];
+  }, [stage]);
+
+  const lineCount = React.useMemo(() => Math.sqrt(tileCount), [tileCount]);
+
+  const handleTileClick = React.useCallback(
+    (tileId: number) => {
+      if (tileId === targetTileId) {
+        // TODO: 다음 스테이지
+        console.log("성공");
+      } else {
+        // TODO: 시간 감소
+      }
+    },
+    [targetTileId],
+  );
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${lineCount}, 1fr)`,
+        gridTemplateRows: `repeat(${lineCount}, 1fr)`,
+        gap: "5px",
+        width: "360px",
+        height: "360px",
+      }}
+    >
+      {Array.from({ length: tileCount }, (_, i) => (
+        <div key={i}>{i}</div>
+      ))}
+    </div>
+  );
+}
